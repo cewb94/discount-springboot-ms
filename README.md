f6a72cd5edfee738fee0bc3412df74ed15719aa1
cewb94 <yaqoubalshatti94@gmail.com>
2026-01-14 13:36:50 +0300

    fixed issue in customer service by removing async since we decided not to use it for simplicity. updateds redeame with sample json and API endpoints

--- README.md
+++ README.md
@@ -14,3 +14,33 @@ docker compose up
 
 To test, use:
 mvn clean test -Dspring.profiles.active=<env>
+
+___________________API___________________
+
+DEV port: 8080, Docker port: 70
+
+localhost:port/api/customers
+localhost:port/api/customers/{id}
+
+localhost:port/api/bills/all
+localhost:port/api/bills/id/{id}
+localhost:port/api/bills/add                -> this one is POST
+
+sample bill request JSON to use in body of POST HTTP
+{
+  "customerId": 1,
+  "lines": [
+    {
+      "itmId": 1,
+      "itmCategory": "GROCERY",
+      "price": 2.00,
+      "quantity": 3
+    },
+    {
+      "itmId": 11,
+      "itmCategory": "NON_GROCERY",
+      "price": 10.00,
+      "quantity": 1
+    }
+  ]
+}
--- src/main/java/com/mycompany/discountms/services/CustomerService.java
+++ src/main/java/com/mycompany/discountms/services/CustomerService.java
@@ -6,7 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 // import org.springframework.cache.annotation.CachePut;
 // import org.springframework.cache.annotation.Cacheable;
-import org.springframework.scheduling.annotation.Async;
+// import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import com.mycompany.discountms.entity.Customer;
@@ -26,22 +26,22 @@ public class CustomerService {
         this.cusRepo = cusRepo;
     }
     
-    @Async
     public List<Customer> getAllCustomers() {
         return cusRepo.findAll();
     }
 
+    // @Async
     // @CachePut(cacheNames = "customers")
     // public CompletableFuture<List<Customer>> getAllCustomers() {
     //     List<Customer> all = cusRepo.findAll();
     //     return CompletableFuture.completedFuture(all);
     // }
 
-    @Async
     public Customer getCustomerById(Long id) {
         return cusRepo.findById(id).orElse(null);
     }
     
+    // @Async
     // public CompletableFuture<Customer> getCustomerById(Long id) {
     //     return CompletableFuture.supplyAsync(() ->
     //             cusRepo.findById(id).orElse(null));
